6e740146d90cc01ae241b28b32fc21432a2e892f
Daniel Calvanese <Daniel.Calvanese@Gmail.com>
2020-11-28 10:46:07 -0500

    Copying the solution because mine isn't accepted by the server.

--- alog.go
+++ alog.go
@@ -30,9 +30,9 @@ func New(w io.Writer) *Alog {
 	}
 	return &Alog{
 		dest:    w,
-		m:       &sync.Mutex{},
 		msgCh:   make(chan string),
 		errorCh: make(chan error),
+		m:       &sync.Mutex{},
 	}
 }
 
@@ -55,9 +55,7 @@ func (al Alog) formatMessage(msg string) string {
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
 	al.m.Lock()
 	defer al.m.Unlock()
-
 	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
-
 	if err != nil {
 		go func(err error) {
 			al.errorCh <- err
